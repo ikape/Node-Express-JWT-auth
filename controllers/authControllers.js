5ddf1769bfd4a6ea6235a24c96088e1f98d86fda
ikape <ikapeisaac3@gmail.com>
2023-01-04 01:06:12 +0100

    second commit

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,4 +1,7 @@
-module.exports.signup_get = (req,res)=>{
+const User = require('../models/users');
+const user = require('../models/users')
+
+module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
 
@@ -6,11 +9,22 @@ module.exports.login_get = (req,res)=>{
     res.render('login');
 }
 
-module.exports.signup_post = (req,res)=>{
-    res.send(' new signup');
+module.exports.signup_post = async (req,res)=>{
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.create({ email, password })
+        res.status(201).json(user)
+
+    } catch (err) {
+        console.log(err);
+        res.status(400).send('error, user not created')
+    }
 }
 
-module.exports.login_post = (req,res)=>{
+module.exports.login_post = async (req,res)=>{
     const {email, password} = req.body;
-    res.send(' user login');
+
+    console.log(email, password);
+    res.send('user login');
 }
